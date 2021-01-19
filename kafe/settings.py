8347d614d4afc9ec7cbee286bfa38c9dcbb4cd2a
Jackey22 <allensiri@naver.com>
2021-01-19 09:44:57 +0900

    profile model form

--- kafe/settings.py
+++ kafe/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'bootstrap4'
+    'bootstrap4',
+    'profile',
 ]
 
 MIDDLEWARE = [
--- kafe/urls.py
+++ kafe/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls')),
+    path('proFile/', include('proFile.urls')),
+
 ]
