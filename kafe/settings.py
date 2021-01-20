e88f355b3990a6eaeaf6c1a1229d284fc7181e02
Jackey22 <allensiri@naver.com>
2021-01-20 15:14:58 +0900

    articles using magic grid

--- kafe/settings.py
+++ kafe/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'account',
     'bootstrap4',
     'proFile',
+    'article',
 ]
 
 MIDDLEWARE = [
--- kafe/urls.py
+++ kafe/urls.py
@@ -21,8 +21,10 @@ from django.urls import path, include
 urlpatterns = [
 
     path('admin/', admin.site.urls),
+
     path('accounts/', include('account.urls')),
     path('profiles/', include('proFile.urls')),
+    path('articles/', include('article.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
