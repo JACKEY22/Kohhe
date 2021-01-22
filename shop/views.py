f310243ef29344b5fea044655779c92deb49ecce
Jackey22 <allensiri@naver.com>
2021-01-23 01:20:17 +0900

    shop detail login requied & success url to detail view

--- shop/views.py
+++ shop/views.py
@@ -1,25 +1,30 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, ListView, DetailView
 
 from shop.forms import ShopCreateForm
 from shop.models import Shop
 
-
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 class ShopCreateView(CreateView):
     model = Shop
     form_class = ShopCreateForm
     template_name = 'shop/create.html'
-    success_url = reverse_lazy('home:home')
 
     def form_valid(self, form):
         temp_form = form.save(commit=False)
         temp_form.owner = self.request.user
         temp_form.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('shop:detail', kwargs={'pk':self.object.pk })
 
 class ShopListView(ListView):
     model = Shop
