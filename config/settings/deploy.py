bd8b8f130e16280a9cd72abd757eb502bd8b92c6
Jackey22 <allensiri@naver.com>
2021-03-15 15:36:26 +0900

    docker secret

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -1,5 +1,12 @@
 from .base import *
 
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -10,7 +17,7 @@ environ.Env.read_env(
 )
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -25,7 +32,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django', # dbname
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb', # network domain - container name
         'PORT': '3306',
     }
